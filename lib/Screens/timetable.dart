7be406f31c2a4a279ce0e2b37c1ecea4366cfcf4
vinayakvijayan <vinayakvijayan107@gmail.com>
2021-06-06 18:05:51 +0530

    edit

--- lib/Screens/timetable.dart
+++ lib/Screens/timetable.dart
@@ -11,44 +11,101 @@ class _TableExample extends State<Timetable> {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('Flutter Table Example'),
+            title: Text('Time Table'),
           ),
-          body: Center(
+          body: 
+        
+          Center(
               child: Column(children: <Widget>[
             Container(
-              margin: EdgeInsets.all(20),
-              child: Table(
-                defaultColumnWidth: FixedColumnWidth(120.0),
-                border: TableBorder.all(
-                    color: Colors.black, style: BorderStyle.solid, width: 2),
-                children: [
-                  TableRow(children: [
-                    Column(children: [
-                      Text('Website', style: TextStyle(fontSize: 20.0))
+              margin: EdgeInsets.all(2),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Table(
+                  
+                  defaultColumnWidth: FixedColumnWidth(80.0),
+                  border: TableBorder.all(
+                      color: Colors.black, style: BorderStyle.solid, width: 1),
+                  children: [
+                    TableRow(children: [
+                      Column(children: [
+                        Text('MONDAY', style: TextStyle(fontSize: 15.0))
+                      ]),
+                      Column(children: [
+                        Text('TUESDAY', style: TextStyle(fontSize: 15.0))
+                      ]),
+                       Column(children: [
+                        Text('WEDNESDAY', style: TextStyle(fontSize: 15.0))
+                      ]),
+                       Column(children: [
+                        Text('THURSDAY', style: TextStyle(fontSize: 15.0))
+                      ]),
+                       Column(children: [
+                        Text('FRIDAY', style: TextStyle(fontSize: 15.0))
+                      ]),
+                     
+                     
                     ]),
-                    Column(children: [
-                      Text('Tutorial', style: TextStyle(fontSize: 20.0))
+                    TableRow(children: [
+                      Column(children: [Text('Javatpoint')]),
+                      Column(children: [Text('Flutter')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                     
                     ]),
-                    Column(children: [
-                      Text('Review', style: TextStyle(fontSize: 20.0))
+                    TableRow(children: [
+                      Column(children: [Text('Javatpoint')]),
+                      Column(children: [Text('MySQL')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      
                     ]),
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Javatpoint')]),
-                    Column(children: [Text('Flutter')]),
-                    Column(children: [Text('5*')]),
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Javatpoint')]),
-                    Column(children: [Text('MySQL')]),
-                    Column(children: [Text('5*')]),
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Javatpoint')]),
-                    Column(children: [Text('ReactJS')]),
-                    Column(children: [Text('5*')]),
-                  ]),
-                ],
+                   
+                    TableRow(children: [
+                      Column(children: [Text('Javatpoint')]),
+                      Column(children: [Text('ReactJS')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                     
+                    ]),
+                    TableRow(children: [
+                      Column(children: [Text('Javatpoint')]),
+                      Column(children: [Text('ReactJS')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      
+                    ]),
+                    TableRow(children: [
+                      Column(children: [Text('Javatpoint')]),
+                      Column(children: [Text('ReactJS')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                     
+                    ]),
+                    TableRow(children: [
+                      Column(children: [Text('Javatpoint')]),
+                      Column(children: [Text('ReactJS')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                     
+                    ]),
+                    TableRow(children: [
+                      Column(children: [Text('Javatpoint')]),
+                      Column(children: [Text('ReactJS')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                      Column(children: [Text('5*')]),
+                     
+                    ]),
+                   
+                  ],
+                ),
               ),
             ),
           ]))),
