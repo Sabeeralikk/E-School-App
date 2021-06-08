a30f4f3c082a5b79ceffe27f451a62ff3f35ad6f
Cyril Shaji <cyrilshaji38@gmail.com>
2021-06-08 22:08:55 +0530

    Sorry for the confusion!

--- CYRIL/E-School-App-main/README.md
+++
@@ -1,4 +0,0 @@
-# School Management App UI Flutter | Backend Firebase
-
-Flutter based School Management App | Flutter | Backend Firebase
-
--- CYRIL/E-School-App-main/android/app/src/main/kotlin/com/example/school_management/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.school_management
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
